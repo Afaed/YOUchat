34e4003d5b54092ba4d2dd0e12edb8a57e749643
Afaed <44733498+Afaed@users.noreply.github.com>
2022-05-16 14:47:59 -0700

    Trying to figure out why the app won't load the video. I might have wrote it down worng in the html

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
-
 let client = AgoraRTC.createClient({mode:'rtc', 'codec':"vp8"})
 
 let config = {
     appid: '6c9cd737debb42b4abfda2e2e6e7842d',
-    token:'0066c9cd737debb42b4abfda2e2e6e7842dIAB0YnhkmUg7uXsVH78YaNsCtr5MebiOHd9qBlcztZEFiL8uuEoAAAAAEAD/8ff9og2EYgEAAQCiDYRi',
+    token:'0066c9cd737debb42b4abfda2e2e6e7842dIAAOroomYU8Dl/WcEA21umrRrVHmVeMMcAzkslhZnhWPo78uuEoAAAAAEAD/8ff9WReEYgEAAQBZF4Ri',
     uid:null,
     channel: 'spooky',
 }
@@ -23,17 +22,34 @@ let localTracks = {
 
 
  // the goal here is WHEN the button is activated, it will join the call. For now lets make a console.log instead
-document.getElementById( 'join', () => {
+document.getElementById( 'join', async () => {
     console.log("${user} wants to join")
+    await joinCall()
 })
 
 //in order for the event to occur, we need a way to fire off the join. So now we make a function called joinCall. We will need to make an async function as well since it will trigger independently
-
 let joinCall = async () => {
-
-    [config.uid, localTracks.audioTracks, localTracks] = await Promise.all([
-        client.join(config.appid, config.channel, config.token)
+    //connects the audio tracks to make it work!
+    [config.uid, localTracks.audioTracks, localTracks.videoTracks] = await Promise.all([
+        client.join(config.appid, config.channel, config.token),
+        //allow the user to listen to the stream and connect to the camera. Allows for video and audio track
+        AgoraRTC.createMicrophoneAudioTrack(),
+        AgoraRTC.createCameraVideoTrack(),
     ])
+    
+//now we need a video player. This also serves as the objet
+
+    let videoPlayer = `<div class = "video-containers" id -= "video-wrapper ${config.uid} 
+    <p class "user-uid" >${config.uid} </p>
+    </div>
+    <div class = "video-player" class = "player" id = "stream-${config.uid}"`
+
+    document.getElementById('stream').insertAdjacentHTML('beforeend', videoPlayer)
+    // allows user to  see the video
+    localTracks.videoTracks.play(`stream ${config.uid}`)
+
+    //executes the object and plays the video.
+    await client.publish([localTracks.audioTracks, localTracks.videoTracks])
 }
 
 //connects to the agora app and its documentaiton.
